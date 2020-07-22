8876035665b71fad9a5b1b154aeddff5cc6a37ab
shoujunw <shoujunw@tujia.com>
2020-07-22 08:42:56 +0800

    windwos

--- algorithm/decisionTree.py
+++ algorithm/decisionTree.py
@@ -10,11 +10,14 @@ import sys
 
 sys.path.append("C:/Users/shoujunw/PycharmProjects/dataMining/utils")
 from pandas import DataFrame
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.tree import DecisionTreeClassifier, export_graphviz, DecisionTreeRegressor
+from sklearn.model_selection import KFold, cross_validate, GridSearchCV, RandomizedSearchCV, cross_val_score
 from utilself import *
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import graphviz
+import numpy as np
+import matplotlib.pyplot as plt
 
 # %%
 # ----------------- 数据集的获取
@@ -50,6 +53,7 @@ Xtrain, Xtest, Ytrain, Ytest
 feature_zh_names = ["头发", "羽毛", "鸡蛋", "牛奶", "机载", "水生", "捕食者", "齿", "骨干", "呼吸", "有毒的", "鳍", "腿", "尾巴", "国内", "catsize"]
 
 # 数据集很巨大的时候,一定要剪枝, 否则,树会一直生长,内存都撑爆
+# 特征必须是数值型**
 decision_tree = DecisionTreeClassifier(
     criterion="gini"  # 标准(不纯度), 每个节点都有一个不纯度 $p(i|t) 标签分类i在节点t上的占比
     # 当决策树欠拟合的时候使用信息熵, 维度大噪音大的时候用gini
@@ -67,7 +71,7 @@ decision_tree = DecisionTreeClassifier(
     # 总数为: 31, 预测正确数量: 23, 预测准确率: 0.7419354838709677
     # 训练集总数为: 70, 预测正确数量: 66, 预测准确率: 0.9428571428571428
 
-    , max_features=7  # 决策树使用的最大特征, 这个一般需要借助pca, 不要轻易尝试, float, sqrt, log2
+    , max_features=7  # 决策树使用的最大特征, 这个一般需要借助pca, 不要轻易尝试, float, sqrt, log2, 默认为特征总个数开平方
     , min_impurity_decrease=0.001  # 信息增益必须>0.01
 
     # 目标权重参数, 设置正样本权重高一些, 完成样本标签平衡的参数, 公式是啥:todo ??
@@ -75,6 +79,12 @@ decision_tree = DecisionTreeClassifier(
     # , min_weight_fraction_leaf 叶子节点最小样本权重,如果小于这个, 则会连同兄弟节点一起被剪枝
 
 )
+
+# friedman_mse: 最小化L2损失
+# mae:绝对平均误差, 最小化L1损失
+# RMSE:均方根误差
+
+DecisionTreeRegressor
 
 decision_tree.fit(Xtrain, Ytrain)
 Ypredict = decision_tree.predict(Xtest)  # True为1, False为0
@@ -110,3 +120,77 @@ dot_data = export_graphviz(
 graph = graphviz.Source(dot_data, encoding="utf-8")
 # graph.format = "png"
 # graph.view("tree") 直接在notebook里面输出有事可以中文的, 奇怪
+
+# 交叉验证, 评估模型的平均准确率, 评估模型的稳定性
+
+# kFold = KFold(n_splits=10, random_state=0)
+decision_tree2 = DecisionTreeClassifier(random_state=0)
+
+# 交叉验证, 1. 使用不同的数据拆分训练模型然后在测试集得分,会得到10个模型
+# cross_val_score, 内部调用的还是cross_validate, 只不过返回的是test_score, 还是会走fit部分的 todo
+result = cross_validate(decision_tree2, df, type_df, cv=10)
+print("=====交叉验证结果")  # test_score字段, 默认调用模型的score方法
+print(result)
+
+# numpy的升维度和降低维度
+a = np.array([1, 2])
+b = np.expand_dims(a, axis=1)
+a = np.array([[1], [2]])
+b = np.squeeze(a)
+
+# 超参数调节
+gridSearchCV = GridSearchCV(DecisionTreeClassifier(),
+                            param_grid={
+                                "max_depth": [3, 4, 5, 6, 7]
+                                , "splitter": ["best", "random"]
+                            }
+                            , cv=10
+                            )
+gridSearchCV.fit(df, type_df)
+print("=======参数搜索结果")
+print(gridSearchCV.best_estimator_, "\n"
+      , gridSearchCV.best_params_, "\n"
+      , gridSearchCV.best_score_, "\n"
+      )  # gridSearchCV.cv_results_ 交叉验证结果,信息太多了,不打印了
+
+# 随机搜索
+randomizedSearchCV = RandomizedSearchCV(
+    DecisionTreeClassifier()
+    , param_distributions={
+        "max_depth": range(3, 100)
+    }
+    , cv=10, n_iter=10)
+randomizedSearchCV.fit(df, type_df)
+print("====随机搜索结果")
+print(randomizedSearchCV.best_params_)  # {'max_depth': 63}
+
+# 模型准确率不高的时候, 查看学习曲线(参数的调整,对score的变化趋势), 查看训练集的分数和测试集的分数
+# tr, te存储分数列表, 横轴为 1,2,3,4 等
+tr = []
+te = []
+for i in range(10):
+    clf = DecisionTreeClassifier(random_state=0
+                                 , max_depth=i + 1
+                                 , criterion="entropy"
+                                 )
+    clf = clf.fit(Xtrain, Ytrain)  # 先拟合
+    score_tr = clf.score(Xtrain, Ytrain)  # 训练集分数
+    score_te = cross_val_score(clf, df, type_df, cv=10).mean()  # 测试集分数采用交叉验证的的平均得分(交叉验证是发生在测试集上的)
+    tr.append(score_tr)
+    te.append(score_te)
+print(max(te))
+plt.plot(range(1, 11), tr, color="red", label="train")
+plt.plot(range(1, 11), te, color="blue", label="test")
+plt.xticks(range(1, 11))  # 设置图的横轴
+plt.legend()
+# plt.show()
+
+# pandas
+# loc[1, "文字列明"] 文字索引 iloc:数字索引
+# 取出所有不等于a的列 df.iloc[:, df.columns != "a"]
+
+print(cross_val_score(DecisionTreeClassifier(random_state=0
+                                             , max_depth=i + 1
+                                             , criterion="entropy"
+                                             )
+                      , df, type_df, cv=10))
