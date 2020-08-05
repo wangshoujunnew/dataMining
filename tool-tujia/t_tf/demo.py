a6397929206ef92efe0348e50a3a827e1cb086fe
shoujunw <shoujunw@tujia.com>
2020-08-06 00:12:33 +0800

    windows

--- tool-tujia/t_tf/demo.py
+++ tool-tujia/t_tf/demo.py
@@ -4,32 +4,36 @@ from tensorflow.python.saved_model import tag_constants
 from tensorflow.python.saved_model.signature_def_utils_impl import predict_signature_def
 
 print(tf.__version__)
+def opDefSelf():
+    """
+    自己定义的一些op操作
+    :return:
+    """
+    c1 = tf.constant([0], name="con1")  # name: con1:0
+    c2 = tf.constant([0], name="con1")  # name: con1_1:1, 快速找到变量对应在图中的位置
+    c3 = tf.constant([0])  # name: Const:0
+    c4 = tf.Variable([0], dtype=dtypes.float32)  # name: Variable:0
+    c5 = tf.Variable([0], dtype=dtypes.float32)  # name: Variable_1:0
+    c6 = tf.Variable([0], dtype=dtypes.float32)  # name: Variable_2:0
 
-c1 = tf.constant([0], name="con1")  # name: con1:0
-c2 = tf.constant([0], name="con1")  # name: con1_1:1, 快速找到变量对应在图中的位置
-c3 = tf.constant([0])  # name: Const:0
-c4 = tf.Variable([0], dtype=dtypes.float32)  # name: Variable:0
-c5 = tf.Variable([0], dtype=dtypes.float32)  # name: Variable_1:0
-c6 = tf.Variable([0], dtype=dtypes.float32)  # name: Variable_2:0
+    myInput = tf.placeholder(dtype=dtypes.float32, shape=[None, 1], name="myInput")
+    y = myInput + 1
+    myOutput = tf.identity(y, name="myOutput")  # 给tensor重命名
 
-myInput = tf.placeholder(dtype=dtypes.float32, shape=[None, 1], name="myInput")
-y = myInput + 1
-myOutput = tf.identity(y, name="myOutput")  # 给tensor重命名
+    g1 = tf.Graph()
+    with g1.as_default():
+        # v1这个张量的名字为v1:0
+        v = tf.get_variable("v1", initializer=tf.ones_initializer(dtype=dtypes.float32), shape=(10, 10))
+        with tf.variable_scope("test"):
+            c7 = tf.constant([0], name="test1")  # name: test/Const:0
+            print("快速获取当前变量的tensor_name =========> ", c7.name)
+            c8 = tf.constant([0])  # name: test/Const_1:0, 快速找到变量对应在图中的位置
+            c9 = tf.get_variable("test2", initializer=tf.zeros_initializer(dtype=dtypes.float32), shape=(10, 10))
 
-g1 = tf.Graph()
-with g1.as_default():
-    # v1这个张量的名字为v1:0
-    v = tf.get_variable("v1", initializer=tf.ones_initializer(dtype=dtypes.float32), shape=(10, 10))
-    with tf.variable_scope("test"):
-        c7 = tf.constant([0], name="test1")  # name: test/Const:0
-        print("快速获取当前变量的tensor_name =========> ", c7.name)
-        c8 = tf.constant([0])  # name: test/Const_1:0, 快速找到变量对应在图中的位置
-        c9 = tf.get_variable("test2", initializer=tf.zeros_initializer(dtype=dtypes.float32), shape=(10, 10))
+    print(g1)
 
-print(g1)
-
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
+# sess = tf.Session()
+# sess.run(tf.global_variables_initializer())
 
 
 def save():
@@ -72,7 +76,7 @@ def saveModelDemo():
 
 # saveModelDemo()
 def loadSaveModel(sess: tf.Session):
-    tf.saved_model.loader.load(sess, export_dir="./saveModel", tags=[tag_constants.SERVING])
+    tf.saved_model.loader.load(sess, export_dir=r"C:\Users\shoujunw\PycharmProjects\dataMining\tool-tujia\t_tf\saveModel", tags=[tag_constants.SERVING])
     graph = tf.get_default_graph()
     x = graph.get_tensor_by_name("myInput:0")
     y = graph.get_tensor_by_name("myOutput:0")
@@ -82,7 +86,7 @@ def loadSaveModel(sess: tf.Session):
     print(yout)
 
 
-loadSaveModel(sess)
+# loadSaveModel(sess)
 
 
 def load():
@@ -120,16 +124,38 @@ def load_bert():
         # tf.get_variable_to_shape_map
 
 
-def getOPname():
-    from tensorflow.python import pywrap_tensorflow
+def getOPname(sess: tf.Session):
+    # bert模型
     checkpoint_path = 'c:/Users/shoujunw/PycharmProjects/dataMining/modelSave/bert/model.ckpt-1286'
-    reader = pywrap_tensorflow.NewCheckpointReader(checkpoint_path)
-    var_to_shape_map = reader.get_variable_to_shape_map()
-    for key in var_to_shape_map:
-        print("tensor_name: ", key)
+    saver = tf.train.import_meta_graph("c:/Users/shoujunw/PycharmProjects/dataMining/modelSave/bert/model.ckpt-1286.meta")
+    saver.restore(sess, checkpoint_path)
+
+    # 打印bert中所有的张亮操作, 顺序是, 前项传播, loss, 优化, 梯度, 反向传播
+    for i, v in list(enumerate(sess.graph.get_operations()))[:100]:
+        print("op{}, value{}".format(i, v.values()))
 
 
-# getOPname()
-# load_bert()
+def loadEstimator():
+    saver = tf.train.import_meta_graph(r"C:\Users\shoujunw\PycharmProjects\dataMining\tool-tujia\t_tf\estimatorModel\model.ckpt-400.meta")
+    with tf.Session() as sess:
+        saver.restore(sess, r"C:\Users\shoujunw\PycharmProjects\dataMining\tool-tujia\t_tf\estimatorModel\model.ckpt-400")
+        for i, v in enumerate(sess.graph.get_operations()):
+            print(i, v.values())
 
-sess.close()
+loadEstimator()
+
+
+def testEstimator():
+
+    es = tf.estimator.BaselineRegressor(
+        model_dir="estimatorModel"
+    )
+    es.train()
+
+
+
+
+# getOPname(sess)
+# # load_bert()
+#
+# sess.close()
