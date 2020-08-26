56466d0697f4c32fbd82339954b0708e4cb6474c
shoujunw <shoujunw@tujia.com>
2020-08-26 21:40:09 +0800

    windows

--- algorithm/lecode.py
+++ algorithm/lecode.py
@@ -42,5 +42,45 @@ def qucickSort(nums, left, right):
     qucickSort(nums, left=mid + 1, right=right)
 
 
-qucickSort(exmaple, 0, len(exmaple) - 1)
-print(exmaple)
+# qucickSort(exmaple, 0, len(exmaple) - 1)
+# print(exmaple)
+
+
+# Z字走法
+def getNext(i, j, nums):
+    # 得到矩阵需要填充的下一个坐标
+    if i <0 and j<0:
+        return 0, 0
+    else:
+        if j % 3 == 0:
+            if i != nums-1:
+                return i+1, j
+            else:
+                return i-1,j+1
+        else:
+            return i - 1, j + 1
+
+def zRoute(ss:str, nums:int):
+    datas = {} # 存储对应坐标点的元素
+    i, j = -1, -1
+
+    for s in ss:
+        nextI, nextJ = getNext(i, j, nums)
+        i=nextI
+        j=nextJ
+        datas[(nextI,nextJ)] = s
+
+    print(datas)
+    maxCol = max(map(lambda x: x[1],list(datas.keys())))
+
+    for i in range(nums):
+        for j in range(maxCol):
+            value = datas.get((i,j))
+            if value == None:
+                print("#", end="") # 坐标点没有的元素采用#填充
+            else:
+                print(value, end="")
+        print("")
+
+
+zRoute("LEETCODEISHIRING", 4)
