e78cfc10adc4e45b4a05216724cb465187701dd6
shoujunw <shoujunw@tujia.com>
2020-08-26 20:57:14 +0800

    windows

--- learn/tensorflowTest.py
+++ learn/tensorflowTest.py
@@ -43,3 +43,42 @@ class TensorflowTest(TestCase):
         print(self.sess.run(loss))
         print("pair对的损失")
         tf.losses.mean_pairwise_squared_error
+
+    # def variable_with_weight_loss(shape, stddev, wl):
+    #     """
+    #     创建一个带l2正则化的shape形状的正太分布权重参数
+    #     :param stddev:
+    #     :param wl:
+    #     :return:
+    #     """
+    #     var = tf.Variable(tf.truncated_normal(shape, stddev=stddev))
+    #
+    #     if wl is not None:
+    #         weights_loss = tf.multiply(tf.nn.l2_loss(var), wl, name="weights_loss")
+    #         tf.add_to_collection("losses", weights_loss)
+    #     return var
+
+    def testConv(self):
+        print("卷积神经网络测试, 图像数据需要reshape到4维度, batch_size, 长, 高, 通道")
+        x = [
+            [1.0, 2.0, 2.0, 3.0]
+        ]
+        x = tf.Variable(x, dtype=tf.float32)
+        x = tf.reshape(x, [-1, 2, 2, 1])
+        filter_w = tf.Variable(tf.truncated_normal([2, 2, 1, 1], stddev=0.1))
+
+        value = tf.nn.conv2d(
+            input=x  # batch_size, 长, 高, 通道
+            , filter=filter_w  # 卷积核的4为数据, [height,width,in_channels,out_channels]
+            , padding='SAME'  # SAME 增加0列或者0行,使得所有的信息都可以利用到, VALID:舍弃
+            , strides=[1, 1, 1, 1]
+        )
+        self.sess.run(tf.global_variables_initializer())
+        print(self.sess.run(value))
+        """ 由于padding, 所以输出的size和input的时候是一样的
+        [[[[0.17726901]
+           [0.13305116]]
+        
+          [[0.01567679]
+           [0.16338262]]]]
+        """
