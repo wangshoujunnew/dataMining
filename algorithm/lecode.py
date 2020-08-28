c9b67d7ddfe2367ab12b6a6f08d6cd728b3a2221
shoujunw <shoujunw@tujia.com>
2020-08-28 08:43:59 +0800

    windows

--- algorithm/lecode.py
+++ algorithm/lecode.py
@@ -1,4 +1,6 @@
 # 快速排序
+from copy import deepcopy
+
 exmaple = [2, 1, 3, 3, 0, 4, 5]
 exmaple = [0]
 
@@ -83,4 +85,42 @@ def zRoute(ss:str, nums:int):
         print("")
 
 
-zRoute("LEETCODEISHIRING", 4)
+# zRoute("LEETCODEISHIRING", 4)
+# 回溯法处理0-1背包
+beibao = [2,1,3,2]
+jiazhi = [12, 10, 20, 15]
+mapDict = dict(zip(beibao, jiazhi))
+C = 5 # 背包容量
+maxValue = -1 # 最大价值
+def bag01(C, wuping_jiazhi, muqianBag):
+    """
+    :param C: 背包的容量
+    :param wuping_jiazhi: 物品及其对应的价值
+    :param muqianBag: 目前背包里面转入的东西
+    :return:
+    """
+    global maxValue
+    if C < 0:
+        return -1
+    if len(wuping_jiazhi) == 0:
+        value = sum(map(lambda x: mapDict.get(x), muqianBag))
+        if value > maxValue:
+            maxValue = value
+            print("over === bag is: {}, maxValue: {}".format(muqianBag, maxValue))
+            # return muqianBag
+        return
+
+    for w, value in wuping_jiazhi.items():
+        # 对每个物品选择是和否
+        tmpDict = deepcopy(wuping_jiazhi)
+        del tmpDict[w]
+
+        bag01(C, tmpDict, muqianBag) # 不选择w
+        # 选择w
+        if C-w >= 0:
+            tmpBag = deepcopy(muqianBag)
+            tmpBag.append(w)
+            bag01(C-w, tmpDict, tmpBag) # 装入w之后剩余的容量
+
+bag01(C, dict(zip(beibao, jiazhi)), [])
+
